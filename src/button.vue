6bd902a2da2a0f9770f05da81da65b5f81730697
rui <946667296@qq.com>
2019-12-21 20:19:55 +0800

    ignore .cache

---
+++ src/button.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button  class="g-button">123</button>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        font: inherit;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
